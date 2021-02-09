e7dd0ab7ed08baf136d48d529502091427c40df7
Rachel Bottino <rachelpbm@gmail.com>
2021-02-09 11:23:59 -0800

    aula 1

--- material/01-formulas-matematicas/tarefa1/solucao.c
+++ material/01-formulas-matematicas/tarefa1/solucao.c
@@ -1,5 +1,23 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 double mycos(double num) {
+
+    double pot = 1;
+    long fac = 1;
+    long exp = 2;
+    double modulo = 1;
+    int maior = 1;
+    while(maior == 1){
+        pot = pot * num * num;
+        fac = fac * exp * (exp-1);
+        exp += 2;     
+        modulo = modulo - (pot/fac);
+        if(modulo < 0.001){
+            printf("É MENOSR\n");
+            maior = 0;
+        }
+        printf("NUMERO: %f\nModulo atual: %f\nPotencia atual: %f\n Fatorial atual: %d\nid EXP: %d\n", num, modulo, pot, fac, exp-2);
+    }
     return 0;
 }
--- material/01-formulas-matematicas/tarefa2/solucao.c
+++ material/01-formulas-matematicas/tarefa2/solucao.c
@@ -1,3 +1,9 @@
-void transforma_log(double *x, double *y, int n) {
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+void transforma_log(double *x, double *y, int n) {
+    for(int i = 1; i < n; i++){
+        y[i] = log((x[i]/x[i-1]));
+    }
 }
